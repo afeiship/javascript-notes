acbef27e4b2225aa0441d4b2f95ceaba8b264c9f
afeiship <1290657123@qq.com>
2019-08-30 16:46:35 +0800

    add: a aboart ajax solution

---
+++ src/2019-08/2019-08-30/perfect-ajax.js
@@ -0,0 +1,22 @@
+// https://stackoverflow.com/questions/32497035/abort-ajax-request-in-a-promise
+
+
+
+// Promise cancellation is currently under specification, there is no built in way to do this yet (it's coming though). We can implement it ourselves:
+
+var validateAjax = function(value) {
+    // remove explicit construction: http://stackoverflow.com/questions/23803743
+    var xhr = $.ajax('data.json', {data: {value: value}}); 
+    var promise = Promise.resolve(xhr).then(function(data){
+         if(!data.isValid) throw new Error(data.message); // throw errors
+         return data;
+    });
+    promise.abort = function(){
+       xhr.abort();
+    });
+    return promise;
+}
+// Now, we can kill the validateAjax calls by calling abort on the promise:
+
+var p = validateAjax("..."); // make request
+p.abort(); // abort it;
