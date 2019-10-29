6bd222b07af75de1059ab20cb74de12c92d75fb5
afeiship <1290657123@qq.com>
2019-10-29 13:07:31 +0800

    add: var update scope

---
+++ daily/2019-10/2019-10-29/js-var1.js
@@ -0,0 +1,18 @@
+// 变量提升以及var对变量提升的
+
+var a = "I'm a in all";
+
+function test1() {
+  console.log(a); 
+  console.log(window.a);
+
+  var a = "I'm a in test1";
+  console.log(a);
+}
+
+test1();
+
+
+// 在 fn test {} 的范围内，有一个局部变量 a （因为 var a 的原因）
+// 全局变量 window.a
+// 由于变量没有赋值之前，默认值为  undefined
